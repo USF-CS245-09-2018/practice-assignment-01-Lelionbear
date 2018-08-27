54cde2bbc12129e8c725ab3284aa108989887ac5
Lelionbear <artvan1000@gmail.com>
2018-08-27 16:40:30 -0700

    Submiting assignment

--- FactorialIterative.java
+++ FactorialIterative.java
@@ -3,9 +3,7 @@ public class FactorialIterative implements Factorial
 public int factorial(int n)
     {
         int fact = 1;
-        for (int x = n; x > 1; x--){
-            fact *= x;
-        }
+        for (int x = n; x > 1; x--) fact *= x;
         return fact;
     }
 }
